04b16a56be9859b1aaabe5b8ed5553147dceea63
shabnam4477 <shabnam.khodadadi@liberis.com>
2025-02-16 20:44:55 +0000

    Add feature selection

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,3 +1,4 @@
+from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 import numpy as np
@@ -15,12 +16,25 @@ class BaseModel:
         mse = mean_squared_error(y_test, predictions)
         return mse
 
-    def cross_validate(self, X, y, cv=5):
-        scores = -1 * cross_val_score(
-            self.model, X, y, cv=cv, scoring="neg_mean_squared_error"
-        )
+    def cross_validate(self, X, y, cv=5, scoring=None):
+        neg = 1
+        if scoring:
+            neg = -1
+        scores = neg * cross_val_score(self.model, X, y, cv=cv, 
+                                       scoring=scoring)
         mean_score = np.mean(scores)
         return mean_score
 
     def calculate_model_score(self, X_test, y_test):
         return self.model.score(X_test, y_test)
+    
+    def feature_selection(self, X_train, y_train, X_test):
+        feature_selector = SelectFromModel(self.model, threshold='median')
+        feature_selector.fit(X_train, y_train)
+        X_train_selected = feature_selector.transform(X_train)
+        X_test_selected = feature_selector.transform(X_test)
+        return X_train_selected, X_test_selected
+
+    @staticmethod
+    def tune_hyperparameters():
+        pass
--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -1,17 +1,14 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 from models.base_model import BaseModel
 
 
 class RandomForestRegressorModel(BaseModel):
-    def __init__(self, hyperparameters):
-        super().__init__(RandomForestRegressor(**hyperparameters))
+    def __init__(self, model):
+        super().__init__(model)
 
-    @staticmethod
     def tune_hyperparameters(parameters, X_train, y_train):
         rf = RandomForestRegressor()
-        grid_search = RandomizedSearchCV(
-            rf, param_distributions=parameters, n_iter=5, cv=5
-        )
+        grid_search = GridSearchCV(rf, param_grid=parameters)
         grid_search.fit(X_train, y_train)
         return grid_search
